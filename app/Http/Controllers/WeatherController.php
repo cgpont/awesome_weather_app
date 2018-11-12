4ed9b1685c8677318407dd1a647e44210de855b3
Garcia <garciac1@eu.hasbro.com>
2018-11-12 11:59:40 +0100

    Added getCitiesListWeather function

--- app/Http/Controllers/WeatherController.php
+++ app/Http/Controllers/WeatherController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use GuzzleHttp\Client;
+use GuzzleHttp\Client; //http://docs.guzzlephp.org
 
 class WeatherController extends Controller
 {
@@ -12,7 +12,8 @@ class WeatherController extends Controller
   private $apiId = 'baede2b33660a266dfd0fb33470868a0';
 
   /**
-   * Gets the weather for a given city.
+   * Gets the weather for a given city id.
+   * Uses library http://docs.guzzlephp.org
    *
    * @param string  $cityId
    * @return \Illuminate\Http\Response
@@ -33,11 +34,42 @@ class WeatherController extends Controller
         return $response;
     } catch(\Exception $e) {
         $errorResponse = array(
-            "apiError" => "There was an error when trying to get the weather from the API",
+            "apiError" => "There was an error when trying to get the city weather from the API",
             "systemErrorMessage" => $e->getMessage(),
             "systemErrorFull" => $e,
         );
         return $errorResponse;
     }
   }
+
+  /**
+   * Gets the weather for all given cities ids.
+   *
+   * @param string  $citiesIds
+   * @return \Illuminate\Http\Response
+   */
+  public function getCitiesListWeather($citiesIds)
+  {
+    try {
+      $client = new Client([
+          'base_uri' => $this->apiBaseUrl,
+          'timeout'  => 2.0,
+      ]);
+      $response = $client->request('GET',
+                                   'group',
+                                   ['query' => ['id' => $citiesIds,
+                                                'appid' => $this->apiId]
+                                   ]);
+      $response = $response->getBody()->getContents();
+      return $response;
+    } catch(\Exception $e) {
+        $errorResponse = array(
+            "apiError" => "There was an error when trying to get the cities weather from the API",
+            "systemErrorMessage" => $e->getMessage(),
+            "systemErrorFull" => $e,
+        );
+        return $errorResponse;
+    }
+  }
+
 }
